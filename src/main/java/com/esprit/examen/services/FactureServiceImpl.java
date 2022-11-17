281dd93fd9d901c842358d67e2b75fcef497879c
AdemYsf <59019101+AdemYoussef@users.noreply.github.com>
2022-11-17 21:45:36 +0100

    correction sonar bug
    A "NullPointerException" could be thrown; "operateur" is nullable here.

--- src/main/java/com/esprit/examen/services/FactureServiceImpl.java
+++ src/main/java/com/esprit/examen/services/FactureServiceImpl.java
@@ -116,6 +116,10 @@ public class FactureServiceImpl implements IFactureService {
 	public void assignOperateurToFacture(Long idOperateur, Long idFacture) {
 		Facture facture = factureRepository.findById(idFacture).orElse(null);
 		Operateur operateur = operateurRepository.findById(idOperateur).orElse(null);
+		if (operateur ==null){
+			log.info("operateur NULL !");
+			throw new NullPointerException();
+		}
 		operateur.getFactures().add(facture);
 		operateurRepository.save(operateur);
 	}
